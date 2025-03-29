a1fc71a4da2d1c88c543d4ac437f254146e2b94c
Vishvanath Madasamy <vishvanathmadasamyofficial@gmail.com>
2025-03-29 19:38:26 -0400

    Update finding the square root of the number.py

--- finding the square root of the number.py
+++ finding the square root of the number.py
@@ -1,7 +1,7 @@
-def newtonsqrt(n):
-    root = n/2
-    for i in range (20):
-        root = (root + n/root)/2
-    print(root)
-n = eval(input("Enter the square root of the number:"))
-newtonsqrt(n)
+def newtonsqrt(n): #defining the variable 
+    root = n/2 
+    for i in range (20): # giving the range value 
+        root = (root + n/root)/2 #applying the root function
+    print(root) #getting the values from the root value
+n = eval(input("Enter the square root of the number:")) #evaluating and printing the values
+newtonsqrt(n) 
