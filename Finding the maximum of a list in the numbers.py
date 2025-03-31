624896b07ec209ec857e6d5007d8d1088b9bbcfb
Vishvanath Madasamy <vishvanathmadasamyofficial@gmail.com>
2025-03-30 22:59:04 -0400

    Update Finding the maximum of a list in the numbers.py

--- Finding the maximum of a list in the numbers.py
+++ Finding the maximum of a list in the numbers.py
@@ -1,6 +1,11 @@
+#List of numbers
 a=[25,9,55,12,5]
+#Initialize max variable to store the maximum numbers
 max=0
+#loop through each element in the list
 for i in a:
-    if (max < i):
-        max = i
+    if (max < i): 
+        max = i#update the max if the current element is greater
+
+#Find and print the maximum number
 print("maximum number=", max)
