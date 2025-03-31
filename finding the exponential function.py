3f7bf702015d14e4bf812cdae95f396272293bcd
Vishvanath Madasamy <vishvanathmadasamyofficial@gmail.com>
2025-03-30 20:33:11 -0400

    Update finding the exponential function.py

--- finding the exponential function.py
+++ finding the exponential function.py
@@ -1,8 +1,11 @@
 def power(base,exp):
+#Base case: If the exponent is 1, return the base
     if(exp==1):
         return (base)
     else:
+        #Recursive case: Mulitply the base with the result of power(base, exp-1)
         return(base*power(base,exp-1))
+#Taking input from the user for the base and exponent
 base=int(input("Enter base:"))
 exp=int(input("Enter the exponential value:"))
 print("Result:", power(base,exp))
