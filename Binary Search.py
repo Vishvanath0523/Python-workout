19f5afae837884902d5e29fdad4876a23187742e
Vishvanath Madasamy <vishvanathmadasamyofficial@gmail.com>
2025-04-01 21:08:47 -0400

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -1,17 +1,17 @@
-a = [30, 20, 40, 60, 70, 69]
-a.sort()
-print(a)
-search=eval(input("enter the element to search:"))
-start=0
-stop=len(a)-1
-while(start<=stop):
-    mid=(start+stop)//2
-    if(search==a[mid]):
-        print("element found at", mid+1)
+a = [30, 20, 40, 60, 70, 69] #create a list of integers
+a.sort() #Sort the list in ascending order (required for binary search)
+print(a) #Print the sorted list
+search=eval(input("enter the element to search:")) # Take the insput fro the user and evaluate it as a python expression
+start=0#intialiaze the start index for binary search
+stop=len(a)-1 #Intialize the stop(end) index for binary search
+while(start<=stop): #Begin the binary search loop while start index is less than or equal to stop loop.
+    mid=(start+stop)//2 #Calculate the middle index
+    if(search==a[mid]): #If middle element matches the search value, print the position (1-based) and exit the loop
+        print("element found at", mid+1) #If the search value is less than the middle element , adjust the stop index to search the left half
         break
-    elif(search<a[mid]):
+    elif(search<a[mid]): #if the search value is greater than the middle element, adjust start index to search the right half
         stop=mid-1
     else:
         start=mid+1
 else:
-    print("element not found")
+    print("element not found") #if the element not found afte the loop ends, print the "Element not found"
