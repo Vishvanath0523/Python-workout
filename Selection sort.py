c47d07cf4b295ff9ee0da057c10a2c3017d3b8fd
Vishvanath Madasamy <vishvanathmadasamyofficial@gmail.com>
2025-04-01 21:22:16 -0400

    Update Selection sort.py

--- Selection sort.py
+++ Selection sort.py
@@ -1,6 +1,6 @@
-a = [16, 10, -1, 369, 5]
-for i in range(len(a)):
-    small = min(a[i:])
-    index = a.index(small)
-    a[i],a[index] = a[index],a[i]
-print(a)
+a = [16, 10, -1, 369, 5] #Create a list of integers
+for i in range(len(a)): # Loop through each indx in the index
+    small = min(a[i:]) # Find the smallest element in the unsorted portion of the list starting from the index i
+    index = a.index(small) # Find the index of that smallest element in the entire list
+    a[i],a[index] = a[index],a[i] #Swap the current element with the smallest element found
+print(a) #After the loop exist ends, print the sorted list
